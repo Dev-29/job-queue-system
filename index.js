94d9d55cbd8ad7d17de94e5ead4020000909b109
Dev Abhiram <devabhiram@gmail.com>
2023-06-26 13:19:27 +0530

    Update by JobID

--- index.js
+++ index.js
@@ -9,9 +9,11 @@ program.version('1.0.0').description('Job Queue System');
 const createCommand = require('./commands/create');
 const listCommand = require('./commands/list');
 const processCommand = require('./commands/process');
+const updateCommand = require('./commands/update');
 
 program.addCommand(createCommand);
 program.addCommand(listCommand);
 program.addCommand(processCommand);
+program.addCommand(updateCommand);
 
 program.parse(process.argv);
